a8e1705f7c9ea69370b2fa57b1c9ad90d887f305
dkearns-lumination <dkearns@lumination.com.au>
2023-08-16 10:59:52 +0930

    Update labs cloud functions to upload files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {credential} from "firebase-admin";
+import {getStorage} from "firebase-admin/storage";
 import applicationDefault = credential.applicationDefault;
+import * as fs from "fs";
 
 export const getCustomToken = functions.https.onRequest((request, response) => {
   if (request.method !== "POST") {
@@ -55,4 +57,52 @@ export const getCustomToken = functions.https.onRequest((request, response) => {
     response.send();
     return;
   });
+});
+
+export const uploadFile = functions.https.onRequest((request, response) => {
+  if (request.method !== "POST") {
+    response.status(405);
+    response.send();
+    return;
+  }
+  if (!request.header("Authorization")) {
+    response.status(422);
+    response.send();
+    return;
+  }
+  if (!request.header("Content-Type") ||
+      request.header("Content-Type") !== "text/plain") {
+    response.status(422);
+    response.send();
+    return;
+  }
+  if (admin.apps.length < 1) {
+    admin.initializeApp({
+      credential: applicationDefault(),
+    });
+  }
+
+  response.header("Access-Control-Allow-Origin", "*");
+
+  admin.auth()
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    .verifyIdToken(request.header("Authorization")
+      .replace("Bearer ", ""))
+    .then((result) => {
+      fs.writeFileSync("temp.txt", request.rawBody);
+      getStorage().bucket("leadme-labs.appspot.com").upload("temp.txt", {
+        destination:
+            `autoLogFiles/${result.uid}/log${Date.now().toString()}.txt`,
+      }).then(() => {
+        response.status(200);
+        response.send();
+        return;
+      });
+    }).catch((error) => {
+      console.log(error);
+      response.status(401);
+      response.send();
+      return;
+    });
 });
